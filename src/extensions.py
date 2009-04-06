ee4e26c49afa666959897c65a4165bfd28841349
bigmonachus <bigmonachus@3b8376e4-d74d-0410-8398-cd281b0a7e60>
2009-04-06 00:59:15 +0000

    oooops.. forgot 2 more modules

---
+++ src/extensions.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+extensions.py
+
+Created by Sergio Gonzalez on 2009-03-20.
+Copyright (c) 2009 __MyCompanyName__. All rights reserved.
+"""
+
+from OpenGL import extensions as exts
+
+#------------------------------
+# Import extensions and add the modules to a list
+#------------------------------
+import OpenGL.GL.EXT.framebuffer_object as fb_obj
+import OpenGL.GL.ARB.depth_texture as depth_texture
+import OpenGL.GL.ARB.multitexture as multitext
+
+extensions = [fb_obj , depth_texture, multitext]
+
+hasExt = {} #Key: extension module. Value: Boolean
+
+def printImportedExts ():
+    print 'Imported extensions'
+    for e in extensions:
+        print e.EXTENSION_NAME
+        
+def checkExtensions():
+    'Fill hasExt map with Booleans for each extension\'s availability '
+    print 'Summary for Extensions:--------------------'
+    for e in extensions:
+        name = e.EXTENSION_NAME
+        hasExt [e] = exts.hasGLExtension (name)
+        print name, hasExt[e]
+    print '------------------------------'
+
+
+
+
